8061642afedcca06535fb60f09a98684e44193ec
Tyger8540 <tymatson2015@gmail.com>
2026-01-03 15:21:24 -0800

    Advanceable Dialogue

--- dress-til-you-drop/scripts/dialogue.gd
+++ dress-til-you-drop/scripts/dialogue.gd
@@ -1,18 +1,24 @@
+class_name Dialogue
 extends RichTextLabel
 
 @export var text_speed: float = 0.05
 @export var text_marker_speed: float = 0.5
 
+var dialogue_array: Array[String]
+var dialogue_index: int = 0
+
 var text_revealing: bool = false
 var text_revealed: bool = false
-var text_timer: float = 1.0
+var text_timer: float = 0.0
 
-var text_marker_timer: float
+var text_marker_timer: float = text_marker_speed
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	text_revealing = true
+	dialogue_array = ["Hello world, this is sample text to test out the dialogue system!!", "Yippeeeeeeee", "bruh", "last one"]
+	text = dialogue_array[dialogue_index]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,3 +37,22 @@ func _process(delta: float) -> void:
 		if text_marker_timer > text_marker_speed:
 			$TextMarker.visible = not $TextMarker.visible
 			text_marker_timer = 0.0
+		
+		if Input.is_action_just_pressed("ui_accept"):
+			continue_dialogue()
+
+
+func continue_dialogue() -> void:
+	visible_ratio = 0.0
+	$TextMarker.visible = false
+	text_revealing = true
+	text_revealed = false
+	dialogue_index += 1
+	if dialogue_index == dialogue_array.size():
+		get_parent().queue_free()
+	else:
+		text = dialogue_array[dialogue_index]
+
+
+func set_dialogue_array(_dialogue_array: Array[String]) -> void:
+	dialogue_array = _dialogue_array
