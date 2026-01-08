d40dbc09740ed8e8b719523ffb72bdeb76dd66d4
Tyger8540 <tymatson2015@gmail.com>
2026-01-07 21:33:20 -0800

    Equip Items

---
+++ dress-til-you-drop/scripts/item.gd
@@ -0,0 +1,19 @@
+extends MeshInstance3D
+
+@export var connected_player_item: MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_static_body_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			connected_player_item.visible = !connected_player_item.visible
