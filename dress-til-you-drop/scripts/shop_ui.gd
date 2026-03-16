1d66b6966eb3fffef967c194484f013b4276ed4c
Tyger8540 <tymatson2015@gmail.com>
2026-03-16 13:18:44 -0700

    Reimported Large Assets

--- dress-til-you-drop/scripts/shop_ui.gd
+++ dress-til-you-drop/scripts/shop_ui.gd
@@ -5,6 +5,10 @@ const SHOP_SLOT_BUTTON = preload("res://scenes/shop_slot_button.tscn")
 @export var stock: Array[InventoryItem]
 @export var stock_purchased: Array[bool]
 
+@export var shop_buttons: Array[ShopSlotButton]
+
+var current_tab_type: Enums.ClothingType = Enums.ClothingType.HAIR
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,3 +26,19 @@ func create_shop_buttons() -> void:
 		$BrowseScreen/Boxes.add_child(shop_button)
 		shop_button.inventory_item = item
 		shop_button.item_icon.texture = item.ui_texture
+
+
+func set_shop_buttons() -> void:
+	for item in stock:
+		if item.item_type == current_tab_type:
+			pass
+
+
+func clear_shop_buttons() -> void:
+	for shop_button in shop_buttons:
+		pass
+
+
+func _on_exit_store_button_up() -> void:
+	# TODO see if this actually works
+	visible = false
